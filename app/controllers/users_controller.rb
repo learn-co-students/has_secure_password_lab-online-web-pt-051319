42af9fd4c097c93ca8a88c0afa599ad5c649e87a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-09-26 19:00:26 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    before_action :require_login, only: [:homepage]
+
+    def new
+        @user = User.new
+    end 
+    
+    def create
+        if params[:user][:password] != params[:user][:password_confirmation]
+            redirect_to signup_path
+        else
+            @user = User.create(user_params)
+            session[:user_id] = @user.id
+            redirect_to root_path
+        end
+    end    
+    
+    def homepage
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    private
+
+    def require_login
+        redirect_to login_path unless session.include? :user_id
+    end
+ 
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
