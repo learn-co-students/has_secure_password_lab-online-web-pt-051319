deed6a70047086e27684d598f59b8be9b91b2c30
Jess Rezac <jess.rezac@gmail.com>
2019-10-20 12:06:00 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: session_params[:name])
+    return head(:forbidden) unless @user.authenticate(session_params[:password])
+    session[:user_id] = @user.id
+  end
+
+  def destroy
+    session.delete(:user_id)
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:name, :password)
+  end
+
+
+end
